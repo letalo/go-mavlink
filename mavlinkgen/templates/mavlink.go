9eb1735c337a6d622f78960b967d7ba469587551
Anton Martsen <amartsen@le-talo.ru>
2015-12-03 17:38:10 +0300

    Comment codegen

--- mavlinkgen/templates/mavlink.go
+++ mavlinkgen/templates/mavlink.go
@@ -1,99 +1,99 @@
-package {{.Name}}
-
-import (
-	"fmt"
-
-	"github.com/SpaceLeap/go-mavlink/mavlink"{{if .Include}}
-	"github.com/SpaceLeap/go-mavlink/mavlink/{{.IncludeName}}"{{end}}
-)
-
-const (
-	PROTOCOL_NAME    = "{{.Name}}"
-	{{if .Version}}PROTOCOL_VERSION = {{.Version}}{{end}}{{if .Include}}
-	PROTOCOL_INCLUDE = {{.IncludeName}}.PROTOCOL_NAME{{end}}
-)
-
-// Init initializes mavlink.ProtocolName, mavlink.ProtocolVersion, and mavlink.MessageFactory.
-func Init() {
-	{{if .Include}}{{.IncludeName}}.Init(){{else}}for i := range mavlink.MessageFactory { mavlink.MessageFactory[i] = nil }{{end}}
-
-	mavlink.ProtocolName = PROTOCOL_NAME
-	{{if .Version}}mavlink.ProtocolVersion = PROTOCOL_VERSION{{end}}
-	
-	{{range .Messages}}
-	mavlink.MessageFactory[{{.ID}}] = func() mavlink.Message { return new({{.Name | UpperCamelCase}}) }{{end}}
-}
-
-// MessageNameIDMap returns a map from message name to message ID.
-func MessageNameIDMap() map[string]int {
-	return map[string]int { {{range .Messages}}
-		"{{.Name}}": {{.ID}},{{end}}
-	}
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// Enums
-////////////////////////////////////////////////////////////////////////////////
-
-{{range .Enums}}
-// {{.Name}}: {{.Description}}
-const ({{range .Entries}}
-	{{.Name}} = {{.Value}} // {{.Description}}{{end}}
-)
-{{end}}
-
-////////////////////////////////////////////////////////////////////////////////
-// Messages
-////////////////////////////////////////////////////////////////////////////////
-
-{{range .Messages}}
-{{$name := .Name | UpperCamelCase}}
-// {{.Description}}
-type {{$name}} struct { {{range .Fields}}
-	{{.Name | UpperCamelCase}} {{.GoType}}{{if .Enum}} `enum:"{{.Enum}}"`{{end}} // {{.Description}}{{end}}
-}
-
-{{if eq $name "Heartbeat"}}
-func NewHeartbeat() *Heartbeat {
-	return &Heartbeat{MavlinkVersion: PROTOCOL_VERSION}
-}
-{{end}}
-func (self *{{$name}}) TypeID() uint8 {
-	return {{.ID}}
-}
-
-func (self *{{$name}}) TypeName() string {
-	return "{{.Name}}"
-}
-
-func (self *{{$name}}) TypeSize() uint8 {
-	return {{.Size}}
-}
-
-func (self *{{$name}}) TypeCRCExtra() uint8 {
-	return {{.CRCExtra}}
-}
-
-func (self *{{$name}}) FieldsString() string {
-	return {{.FieldsString}}
-}
-
-func (self *{{$name}}) String() string {
-	return mavlink.NameIDFromMessage(self) + "{" + self.FieldsString() + "}"
-}
-{{end}}
-
-{{if .StringSizes}}
-////////////////////////////////////////////////////////////////////////////////
-// String Helpers
-////////////////////////////////////////////////////////////////////////////////
-
-{{range $size, $_ := .StringSizes}}
-type Char{{$size}}[{{$size}}]byte
-
-func (chars *Char{{$size}}) String() string {
-	return mavlink.FixString(chars[:])
-}
-{{end}}
-
-{{end}}
+// package {{.Name}}
+//
+// import (
+// 	"fmt"
+//
+// 	"github.com/letalo/go-mavlink/mavlink"{{if .Include}}
+// 	"github.com/letalo/go-mavlink/mavlink/{{.IncludeName}}"{{end}}
+// )
+//
+// const (
+// 	PROTOCOL_NAME    = "{{.Name}}"
+// 	{{if .Version}}PROTOCOL_VERSION = {{.Version}}{{end}}{{if .Include}}
+// 	PROTOCOL_INCLUDE = {{.IncludeName}}.PROTOCOL_NAME{{end}}
+// )
+//
+// // Init initializes mavlink.ProtocolName, mavlink.ProtocolVersion, and mavlink.MessageFactory.
+// func Init() {
+// 	{{if .Include}}{{.IncludeName}}.Init(){{else}}for i := range mavlink.MessageFactory { mavlink.MessageFactory[i] = nil }{{end}}
+//
+// 	mavlink.ProtocolName = PROTOCOL_NAME
+// 	{{if .Version}}mavlink.ProtocolVersion = PROTOCOL_VERSION{{end}}
+//
+// 	{{range .Messages}}
+// 	mavlink.MessageFactory[{{.ID}}] = func() mavlink.Message { return new({{.Name | UpperCamelCase}}) }{{end}}
+// }
+//
+// // MessageNameIDMap returns a map from message name to message ID.
+// func MessageNameIDMap() map[string]int {
+// 	return map[string]int { {{range .Messages}}
+// 		"{{.Name}}": {{.ID}},{{end}}
+// 	}
+// }
+//
+// ////////////////////////////////////////////////////////////////////////////////
+// // Enums
+// ////////////////////////////////////////////////////////////////////////////////
+//
+// {{range .Enums}}
+// // {{.Name}}: {{.Description}}
+// const ({{range .Entries}}
+// 	{{.Name}} = {{.Value}} // {{.Description}}{{end}}
+// )
+// {{end}}
+//
+// ////////////////////////////////////////////////////////////////////////////////
+// // Messages
+// ////////////////////////////////////////////////////////////////////////////////
+//
+// {{range .Messages}}
+// {{$name := .Name | UpperCamelCase}}
+// // {{.Description}}
+// type {{$name}} struct { {{range .Fields}}
+// 	{{.Name | UpperCamelCase}} {{.GoType}}{{if .Enum}} `enum:"{{.Enum}}"`{{end}} // {{.Description}}{{end}}
+// }
+//
+// {{if eq $name "Heartbeat"}}
+// func NewHeartbeat() *Heartbeat {
+// 	return &Heartbeat{MavlinkVersion: PROTOCOL_VERSION}
+// }
+// {{end}}
+// func (self *{{$name}}) TypeID() uint8 {
+// 	return {{.ID}}
+// }
+//
+// func (self *{{$name}}) TypeName() string {
+// 	return "{{.Name}}"
+// }
+//
+// func (self *{{$name}}) TypeSize() uint8 {
+// 	return {{.Size}}
+// }
+//
+// func (self *{{$name}}) TypeCRCExtra() uint8 {
+// 	return {{.CRCExtra}}
+// }
+//
+// func (self *{{$name}}) FieldsString() string {
+// 	return {{.FieldsString}}
+// }
+//
+// func (self *{{$name}}) String() string {
+// 	return mavlink.NameIDFromMessage(self) + "{" + self.FieldsString() + "}"
+// }
+// {{end}}
+//
+// {{if .StringSizes}}
+// ////////////////////////////////////////////////////////////////////////////////
+// // String Helpers
+// ////////////////////////////////////////////////////////////////////////////////
+//
+// {{range $size, $_ := .StringSizes}}
+// type Char{{$size}}[{{$size}}]byte
+//
+// func (chars *Char{{$size}}) String() string {
+// 	return mavlink.FixString(chars[:])
+// }
+// {{end}}
+//
+// {{end}}
